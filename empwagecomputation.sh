487e705dc17a1d1d5ac27b4a0aa4437dd07ca258
[Digvikadam] <[digvikadam168@gmail.com]>
2021-09-25 18:28:23 +0530

    [Digvijay] added:empAttendance Feature

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -1,4 +1,19 @@
 #! /bin/bash -x
 
 
-echo "welcome to empwage"
+echo "welcome to empwageAttendace problem"
+#variable
+empcheck=$((RANDOM%2)) # 0/1
+#constant variable
+IS_PRESENT=1
+
+
+#selection
+if [ $empcheck -eq $IS_PRESENT ]
+then
+        echo "emp is present"
+else
+        echo "emp is absent"
+fi
+
+
