43a3d6df2961562a88631700b14137f6134d2b7e
[Digvikadam] <[digvikadam168@gmail.com]>
2021-09-25 18:47:15 +0530

    [Digvijay] added:empwagecalculate

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -6,13 +6,16 @@ echo "welcome to empwageAttendace problem"
 empcheck=$((RANDOM%2)) # 0/1
 #constant variable
 IS_PRESENT=1
-
+emprate=20
+empHrs=8
 
 #selection
 if [ $empcheck -eq $IS_PRESENT ]
 then
+        salary=$((empHrs * EMP_RATE_PER_HR))
         echo "emp is present"
 else
+	salary=0
         echo "emp is absent"
 fi
 
